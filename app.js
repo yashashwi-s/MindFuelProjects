2398bc76e32a57811ea94a7113e8a4873551194a
yashashwi-s <yashashwi.singhania.bce23@itbhu.ac.in>
2024-04-11 23:36:28 +0530

    fixed some minor issues with the client routers

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const Student = require("./models/Student");
 const Client = require("./models/Client");
 const Application = require("./models/Application");
 
-var t = 0;
+var t=0;
 let user;
 app.use(express.json());
 
@@ -51,13 +51,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  if (!req.session) {
-    return next(new Error("Oh no"));
-  }
-  next();
-});
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Connected to MongoDB!");
@@ -66,36 +59,33 @@ db.once("open", function () {
 module.exports = db;
 
 app.use(function(req, res, next) {
-  if (req.session.userId) {
-    t = 1;
-  } else {
-    t = 0;
-  }
+  req.session.t = req.session.userId ? 1 : 0;
   next();
 });
 
+
 app.get("/", function (req, res) {
-  res.render("home", { t: t });
+  res.render("home", { t: req.session.t });
 });
 
 app.get("/home", function (req, res) {
-  res.render("home", { t: t });
+  res.render("home", { t: req.session.t });
 });
 
 app.post("/", function (req, res) {
-  res.render("home", { t: t });
+  res.render("home", { t: req.session.t });
 });
 
 app.post("/#", function (req, res) {
-  res.render("home", { t: t });
+  res.render("home", { t: req.session.t });
 });
 
 app.get("/signup", function (req, res) {
-  res.render("signup", { t: t });
+  res.render("signup", { t: req.session.t });
 });
 
 app.get("/login", function (req, res) {
-  res.render("login", { t: t });
+  res.render("login", { t: req.session.t });
 });
 
 app.post("/applyToProject", async (req, res) => {
@@ -139,7 +129,7 @@ app.get("/projectApplications/:projectID", async (req, res) => {
 });
 
 if (t === 0) {
-  console.log(t);
+  
   app.post("/signup", async function (req, res) {
     const {
       name,
@@ -214,16 +204,18 @@ app.post("/login", async function (req, res) {
   try {
     user = await Student.findOne({ username: loginUsername });
 
-    console.log(user);
-    console.log(loginUsername);
     if (!user) {
       console.log("User not found");
-      return res.render("login", { error: "Invalid username or password" });}
+      return res.render("login", { error: "Invalid username or password" });
+    }
+
     const isPasswordMatch = await bcrypt.compare(loginPassword, user.password);
 
     if (isPasswordMatch) {
       req.session.userId = user._id;
-      req.app.locals.user = user;
+      req.session.user = user;
+      req.session.t = 1;
+      console.log("Session after login:", req.session);
       res.redirect("/");
     } else {
       console.log("Invalid password");
@@ -234,66 +226,69 @@ app.post("/login", async function (req, res) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 app.get("/profile", function (req, res) {
-  if (t === 1 && user) {
-    res.render("profile", { t: t, user: user });
+  if (req.session.t === 1 && req.session.user) {
+    res.render("profile", { t: req.session.t, user: user });
   } else {
     res.redirect("/signup");
   }
 });
-// app.js
 
-if (t === 0) {
-  app.get("/client", function (req, res) {
-      res.render("client", { t: t, error: null });
-  });
+app.get("/client", function (req, res) {
+  if (req.session.t === 0) {
+    res.render("client", { t: req.session.t, error: null });
+  } else {
+    res.redirect("/profile");
+  }
+});
 
-  app.post("/client", async function (req, res) {
-      const {
-          organization,
-          contact,
-          email,
-          website,
-          password,
-          confirmPassword
-      } = req.body;
+app.post("/client", async function (req, res) {
+  if (req.session.t === 0) {
+    const {
+      organization,
+      contact,
+      email,
+      website,
+      password,
+      confirmPassword
+    } = req.body;
 
-      try {
-          const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-          const newClient = new Client({
-              organization,
-              contact,
-              email,
-              website,
-              password: hashedPassword
-          });
+      const newClient = new Client({
+        organization,
+        contact,
+        email,
+        website,
+        password: hashedPassword
+      });
 
-          await newClient.save();
-          res.redirect("/");
-      } catch (error) {
-          console.error(error);
-          res.status(500).json({ error: "Failed to create client" });
-      }
-  });
-} else {
-  app.get("/client", function(req, res){
-      res.redirect("\profile");
-  });
-}
+      await newClient.save();
+      res.redirect("/");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to create client" });
+    }
+  } else {
+    res.redirect("/profile");
+  }
+});
+
 
 app.get("/projects", async function(req,res){
     const clients = await Client.find({});
-    res.render("projects", {t:t, clients: clients})
+    res.render("projects", {t:req.session.t, clients: clients})
 })
 app.get("/sendEmail", function (req, res) {
-  res.render("sendEmail", { t: t });
+  res.render("sendEmail", { t: req.session.t });
 });
 
 app.get("/student", async function (req, res) {
   try {
     const users = await Student.find({});
-    res.render("student", { t: t, users: users });
+    res.render("student", { t: req.session.t, users: users });
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -302,7 +297,7 @@ app.get("/student", async function (req, res) {
 
 app.get("/chat/:userId", function(req, res) {
     const userId = req.params.userId;
-    res.render("chatRoom", { t:t, userId: userId });
+    res.render("chatRoom", { t:req.session.t, userId: userId });
 });
 
 app.listen(3000, function () {
